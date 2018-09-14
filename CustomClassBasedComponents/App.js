fa4c9014d2677f66670621a77c293ae127df1c13
Ibrar Ul Haq <abrarulhaq.ios@gmail.com>
2018-09-14 12:38:36 +0500

    Show Modal View on Click List Item and Close on click close/back button and also delete item on delete button press from modal view and close modal view with slide animation on close/delete listitem

--- CustomClassBasedComponents/App.js
+++ CustomClassBasedComponents/App.js
@@ -4,17 +4,38 @@ import { StyleSheet, View } from "react-native";
 import PlaceInput from "./src/components/PlaceInput/PlaceInput";
 import PlaceList from "./src/components/PlaceList/PlaceList";
 import PlaceImage  from "./src/assets/beautiful-place.jpg";
+import PlaceDetail from "./src/components/PlaceDetail/PlaceDetail";
 
 export default class App extends Component {
   state = {
-    places: []
+    places: [],
+    selectedPlace : null
   };
+
+  modalClosedHandler = () => {
+    this.setState({
+      selectedPlace : null
+    }
+
+    );
+  };
+
+  placeDeleteHandler = () => {
+    this.setState(prevState => {
+      return {
+        places: prevState.places.filter(place => {
+          return place.key !== prevState.selectedPlace.key;
+        }),
+        selectedPlace: null
+      };
+    });
+  }
 
   placeAddedHandler = placeName => {
     this.setState(prevState => {
       return {
         places: prevState.places.concat({
-          key: Math.random(),
+          key: Math.random().toString(),
           name: placeName,
           image: {
             uri:"http://www.city-data.com/forum/attachments/world/99894d1345310917-most-beautiful-place-world-altay4.jpg"
@@ -24,24 +45,31 @@ export default class App extends Component {
     });
   };
 
-  placeDeletedHandler = key => {
-    this.setState(prevState => {
-      return {
-        places: prevState.places.filter(place => {
-          return place.key !== key;
+  placeSeletedHandler = key => {
+
+    this.setState(prevState =>{
+      return{
+        selectedPlace : prevState.places.find(place =>{
+          return place.key === key;  
         })
       };
     });
+
+    
   };
 
   render() {
     return (
       <View style={styles.container}>
+
         <PlaceInput onPlaceAdded={this.placeAddedHandler} />
         <PlaceList
           places={this.state.places}
-          onItemDeleted={this.placeDeletedHandler}
+          onItemSeleted={this.placeSeletedHandler}
         />
+        <PlaceDetail selectedPlace= {this.state.selectedPlace} 
+        onItemDeleted ={this.placeDeleteHandler} 
+        onModalClosed={this.modalClosedHandler}/>
       </View>
     );
   }
--- CustomClassBasedComponents/src/components/PlaceList/PlaceList.js
+++ CustomClassBasedComponents/src/components/PlaceList/PlaceList.js
@@ -14,7 +14,7 @@ const placeList = props => {
             <ListItem 
              placeName = {info.item.name} 
              placeImage = {info.item.image}
-             onItemPressed = {() => props.onItemDeleted(info.item.key)}
+             onItemPressed = {() => props.onItemSeleted(info.item.key.toString())}
         />
         )}
         />
