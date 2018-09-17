ab96416db7600758cfc1466054e12e727980a7c9
Ibrar Ul Haq <abrarulhaq.ios@gmail.com>
2018-09-17 10:39:15 +0500

    redux configuration with react-native

---
+++ CustomClassBasedComponents/src/store/reducers/places.js
@@ -0,0 +1,14 @@
+import {ADD_PLACE,DELETE_PLACE,SELECT_PLACE,DESELECT_PLACE} from '../actions/actionTypes'
+
+const initialState = {
+    places: [],
+    selectedPlace : null
+};
+const reducer = (state = initialState, action) =>{
+    switch(action.type){
+        default:
+        return state;
+    }
+};
+
+export default reducer;
