4b711a34951042b4e3c545e0a02a344bb85d62e1
Ibrar Ul Haq <abrarulhaq.ios@gmail.com>
2018-10-17 16:06:11 +0500

    added launch icon and splash screen for ios

--- CustomClassBasedComponents/android/app/src/main/java/com/customclassbasedcomponents/MainActivity.java
+++ CustomClassBasedComponents/android/app/src/main/java/com/customclassbasedcomponents/MainActivity.java
@@ -1,8 +1,11 @@
 package com.customclassbasedcomponents;
-
+import android.widget.LinearLayout;
+import android.graphics.Color;
+import android.widget.TextView;
+import android.view.Gravity;
+//import android.util.TypedText;
 import com.reactnativenavigation.controllers.SplashActivity;
 
 public class MainActivity extends SplashActivity {
-
-
+    
 }
